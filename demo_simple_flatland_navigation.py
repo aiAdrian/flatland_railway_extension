1cb2b052b1b9e9efbd4a156653ff9710b67c8a86
adrian_egli <adrian.egli@gmail.com>
2022-09-02 11:44:05 +0200

    version has still many TODOs - but it might works.

--- demo_simple_flatland_navigation.py
+++ demo_simple_flatland_navigation.py
@@ -67,7 +67,7 @@ def run_simulation(flatland_environment_helper: FlatlandEnvironmentHelper,
             env.dev_obs_dict.update(
                 {agent_handle: flatland_resource_allocator.get_assigned_resources(agent_handle=agent_handle)})
 
-        if step % 100 == 0:
+        if step % 10 == 0:
             flatland_renderer.render(show_observations=True)
             time.sleep(0.01)
 
