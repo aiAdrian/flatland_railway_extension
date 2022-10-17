fbb1634e03eefcf57c185b279b8e82ff3ccd6e43
adrian_egli <adrian.egli@gmail.com>
2022-10-17 08:44:08 +0200

    Improved flatland rendering method - user can set properties such as - show_observations - show_predictions - disable_background_rendering - show_rowcols

--- demo_flatland_extensions.py
+++ demo_flatland_extensions.py
@@ -6,9 +6,9 @@ import time
 from data_exports.FlatlandRecifeExporter import FlatlandRecifeExporter
 from flatland_extensions.FlatlandEnvironmentHelper import FlatlandEnvironmentHelper
 from flatland_extensions.FlatlandGraphBuilder import FlatlandGraphBuilder
-from flatland_extensions.utils.FlatlandRenderer import FlatlandRenderer
 from flatland_extensions.RailroadSwitchAnalyser import RailroadSwitchAnalyser
 from flatland_extensions.RailroadSwitchCluster import RailroadSwitchCluster
+from flatland_extensions.utils.FlatlandRenderer import FlatlandRenderer
 
 
 def print_agent_information(
@@ -53,7 +53,7 @@ railroad_switch_analyser.prepare_observation_data_plot()
 railroad_switch_analyser.do_debug_plot()
 
 flatland_renderer = FlatlandRenderer(env=flatland_environment_helper.get_rail_env())
-flatland_renderer.start_render_loop()
+flatland_renderer.start_render_loop(show_rowcols=True, disable_background_rendering=True)
 flatland_renderer.close()
 
 print_agent_information(flatland_environment_helper, railroad_switch_analyser)
@@ -69,7 +69,7 @@ for agent_handle in flatland_environment_helper.get_rail_env().get_agent_handles
     flatland_environment_helper.get_rail_env().agents[agent_handle].position = agent_pos
     flatland_graph_builder.prepare_observation_data_plot(agent_pos, agent_dir, agent_target)
 
-    flatland_renderer.render()
+    flatland_renderer.render(show_rowcols=True, disable_background_rendering=True)
     time.sleep(0.5)
 
     flatland_environment_helper.get_rail_env().agents[agent_handle].position = None
