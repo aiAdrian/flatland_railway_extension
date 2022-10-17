46e1a50e7bfb0f59ab3af7954d341ffb7e614c15
adrian_egli <adrian.egli@gmail.com>
2022-10-17 22:34:32 +0200

    Refactoring code to conform to Python Camel Case

--- flatland_extensions/environment_extensions/DynamicsResourceData.py
+++ flatland_extensions/environment_extensions/DynamicsResourceData.py
@@ -7,11 +7,11 @@ class DynamicsResourceData:
     def __init__(self, res: Tuple[int, int], infrastructure_data: InfrastructureData):
         self.gradient: float = 0.0
         self.distance: float = 400
-        self.vMax: float = 200 / 3.6
+        self.max_velocity: float = 200 / 3.6
         self.backward = False
         self.random_braking_factor = 1.0
 
         if infrastructure_data is not None:
             self.distance = infrastructure_data.get_cell_length(res)
-            self.vMax = infrastructure_data.get_velocity(res)
+            self.max_velocity = infrastructure_data.get_velocity(res)
             self.gradient = infrastructure_data.get_gradient(res)
