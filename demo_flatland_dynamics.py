68691301f3f2c0d841a1a997a65feb4604822e49
adrian_egli <adrian.egli@gmail.com>
2022-11-18 14:36:53 +0100

    the demo is now more generalized to use - user can just replace the "class" type of the environment: RailEnv, XRailEnv or FlatlandDynamics - and the simulation/demostration will handle the behavior automatically.

--- demo_flatland_dynamics.py
+++ demo_flatland_dynamics.py
@@ -1,4 +1,5 @@
 import time
+from ctypes import Union
 
 import numpy as np
 from flatland.envs.rail_env import RailEnv
@@ -7,10 +8,13 @@ from flatland.envs.rail_env_action import RailEnvActions
 from flatland_extensions.FlatlandEnvironmentHelper import FlatlandEnvironmentHelper
 from flatland_extensions.RailroadSwitchAnalyser import RailroadSwitchAnalyser
 from flatland_extensions.RailroadSwitchCluster import RailroadSwitchCluster
+from flatland_extensions.environment_extensions.DynamicAgent import DynamicAgent
 from flatland_extensions.environment_extensions.FlatlandDynamics import FlatlandDynamics
 from flatland_extensions.environment_extensions.FlatlandResourceAllocator import FlatlandResourceAllocator
 from flatland_extensions.environment_extensions.InfrastructureData import InfrastructureData
+from flatland_extensions.environment_extensions.XRailEnv import XRailEnv
 from flatland_extensions.utils.FlatlandDynamicsRenderer import FlatlandDynamicsRenderer
+from flatland_extensions.utils.FlatlandRenderer import FlatlandRenderer
 
 
 def create_infrastructure_data(env: RailEnv, railroad_switch_analyser: RailroadSwitchAnalyser) -> InfrastructureData:
@@ -31,70 +35,92 @@ def create_infrastructure_data(env: RailEnv, railroad_switch_analyser: RailroadS
 def run_simulation(flatland_environment_helper: FlatlandEnvironmentHelper,
                    railroad_switch_cluster: RailroadSwitchCluster,
                    railroad_switch_analyser: RailroadSwitchAnalyser,
-                   enable_moving_block_resource_allocation_strategy=False):
+                   enable_moving_block_resource_allocation_strategy=False,
+                   enable_rendering=False):
     env: FlatlandDynamics = flatland_environment_helper.get_rail_env()
     observations, info = env.reset()
 
-    flatland_renderer = FlatlandDynamicsRenderer(
-        env=flatland_environment_helper.get_rail_env(),
-        show_debug=True,
-        show_agents=False)
+    flatland_renderer = None
+    if enable_rendering:
+        if isinstance(env, FlatlandDynamics):
+            flatland_renderer = FlatlandDynamicsRenderer(
+                env=flatland_environment_helper.get_rail_env(),
+                show_debug=True,
+                show_agents=False)
+        else:
+            flatland_renderer = FlatlandRenderer(
+                env=flatland_environment_helper.get_rail_env(),
+                show_debug=True,
+                show_agents=True)
 
     # Create a test infrastructure
     # ---------------------------------------------------------------------------------------------------------------
     # share the infrastructure with the agents ( train runs)
     for agent in env.agents:
-        agent.set_infrastructure_data(
-            create_infrastructure_data(env, railroad_switch_analyser)
-        )
-        agent.rolling_stock.set_max_braking_acceleration(-0.15)
-        agent.set_mass(500)
-        agent.set_tractive_effort_rendering(False)
+        if isinstance(agent, DynamicAgent):
+            agent.set_infrastructure_data(
+                create_infrastructure_data(env, railroad_switch_analyser)
+            )
+            agent.rolling_stock.set_max_braking_acceleration(-0.15)
+            agent.set_mass(500)
+            agent.set_tractive_effort_rendering(False)
 
     # ---------------------------------------------------------------------------------------------------------------
     # Start simulation
     # ---------------------------------------------------------------------------------------------------------------
-    flatland_resource_allocator: FlatlandResourceAllocator = env.get_active_flatland_resource_allocator()
-    flatland_resource_allocator.set_minimal_free_time_to_reallocate_other_agent(120)
+    flatland_resource_allocator: Union(FlatlandResourceAllocator, None) = None
+    if isinstance(env, XRailEnv):
+        flatland_resource_allocator = env.get_active_flatland_resource_allocator()
+        if flatland_resource_allocator is not None:
+            flatland_resource_allocator.set_minimal_free_time_to_reallocate_other_agent(120)
 
     for step in range(10000):
-        flatland_renderer.set_flatland_resource_allocator(flatland_resource_allocator)
+        if flatland_renderer is not None and isinstance(flatland_renderer, FlatlandDynamicsRenderer):
+            flatland_renderer.set_flatland_resource_allocator(flatland_resource_allocator)
         if not enable_moving_block_resource_allocation_strategy:
-            env.activate_railroad_switch_cluster_locking(railroad_switch_cluster)
+            if isinstance(env, XRailEnv):
+                env.activate_railroad_switch_cluster_locking(railroad_switch_cluster)
 
         actions = {}
         for agent_handle in env.get_agent_handles():
             obs = observations[agent_handle]
             actions.update({agent_handle: RailEnvActions(obs[0])})
-
         observations, all_rewards, dones, info = env.step(actions)
 
-        if step % 10 == 0:
-            flatland_renderer.render(show_observations=True, disable_background_rendering=True)
-            time.sleep(0.01)
+        if flatland_renderer is not None:
+            if step % 10 == 0 or not isinstance(env, FlatlandDynamics):
+                flatland_renderer.render(show_observations=True, disable_background_rendering=True)
+                time.sleep(0.01)
 
         if dones["__all__"]:
             break
-        if flatland_renderer.is_closed():
-            break
 
-    if not flatland_renderer.is_closed():
-        print("Please close render window to exit")
-        flatland_renderer.start_render_loop()
-    flatland_renderer.close()
+        if flatland_renderer is not None:
+            if flatland_renderer.is_closed():
+                break
 
-    env.agents[3].do_debug_plot(1, 1, True)
-    for i_agent, agent in enumerate(env.agents):
-        n_agents = env.get_num_agents()
-        agent.do_debug_plot(i_agent + 1, n_agents, i_agent + 1 == n_agents, i_agent == 0)
+    if flatland_renderer is not None:
+        if not flatland_renderer.is_closed():
+            print("Please close render window to exit")
+            flatland_renderer.start_render_loop()
+        flatland_renderer.close()
+
+    if enable_rendering:
+        if isinstance(env, FlatlandDynamics):
+            env.agents[3].do_debug_plot(1, 1, True)
+            for i_agent, agent in enumerate(env.agents):
+                n_agents = env.get_num_agents()
+                agent.do_debug_plot(i_agent + 1, n_agents, i_agent + 1 == n_agents, i_agent == 0)
 
 
 # -----------------------------------------------------------------------------------------------------------------
-flatland_environment_helper = FlatlandEnvironmentHelper(rail_env=FlatlandDynamics, random_seed=2341)
+flatland_environment_helper = FlatlandEnvironmentHelper(rail_env=FlatlandDynamics,
+                                                        random_seed=2341)
 railroad_switch_analyser = RailroadSwitchAnalyser(env=flatland_environment_helper.get_rail_env())
 railroad_switch_cluster = RailroadSwitchCluster(railroad_switch_analyser=railroad_switch_analyser)
 
 run_simulation(flatland_environment_helper,
                railroad_switch_cluster,
                railroad_switch_analyser,
-               enable_moving_block_resource_allocation_strategy=False)
+               enable_moving_block_resource_allocation_strategy=False,
+               enable_rendering=True)
