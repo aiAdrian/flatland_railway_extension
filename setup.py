47e86ea4015bfccc9424ef3aecd0ba7e94ac23dc
adrian_egli <adrian.egli@gmail.com>
2022-12-21 22:13:54 +0100

    pip install flatland-railway-extension is working > python setup.py sdist > twine upload dist/*

--- setup.py
+++ setup.py
@@ -1,29 +1,23 @@
+# python setup.py sdist
+# pip install twine
+# twine upload dist/*
+
 import os
 
 from setuptools import find_packages
 from setuptools import setup
 
 
-def read_text(file_name: str):
-    return open(os.path.join('.', file_name)).read()
-
-with open('README.md', 'r', encoding='utf8') as readme_file:
-    readme = readme_file.read()
-
-
 setup(
     name='flatland-railway-extension',
-    version='0.1.0',
+    version='0.1.1',
     author='Adrian Egli',
     author_email = "3dhelp@gmail.com",
     description='Extends Flatland Railway Simulator with helpful features.',
-    long_description=readme,
-    long_description_content_type="text/markdown",
     url='https://github.com/aiAdrian/flatland_railway_extension',
     keywords='flatland, railway, extension, dynamics, simulation, multi-agent, reinforcement learning',
     python_requires='>=3.6, <4',
     packages=find_packages('.'),
-    license=read_text("LICENSE"),
     install_requires=[
         'flatland-rl>=3.0.15',
         'numpy',
