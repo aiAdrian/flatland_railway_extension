5517bfa048998ae04d48fb3bc09787af8aae9070
adrian_egli <adrian.egli@gmail.com>
2022-12-21 15:21:07 +0100

    setup.py changed

--- setup.py
+++ setup.py
@@ -7,27 +7,26 @@ from setuptools import setup
 def read_text(file_name: str):
     return open(os.path.join('.', file_name)).read()
 
+with open('README.md', 'r', encoding='utf8') as readme_file:
+    readme = readme_file.read()
+
 
 setup(
-    name='flatland_railway_extension',
+    name='flatland-railway-extension',
     version='0.1.0',
     author='Adrian Egli',
     author_email = "3dhelp@gmail.com",
     description='Extends Flatland Railway Simulator with helpful features.',
-    long_description='',
+    long_description=readme,
+    long_description_content_type="text/markdown",
     url='https://github.com/aiAdrian/flatland_railway_extension',
     keywords='flatland, railway, extension, dynamics, simulation, multi-agent, reinforcement learning',
     python_requires='>=3.6, <4',
-    packages=find_packages(include=['flatland_railway_extension', 'flatland_railway_extension.*']),
+    packages=find_packages('.'),
     license=read_text("LICENSE"),
     install_requires=[
         'flatland-rl>=3.0.15',
         'numpy',
         'matplotlib',
-    ],
-    entry_points={
-        'runners': [
-            'demo_flatland_dynamics=demo_flatland_dynamics.py',
-        ]
-    }
+    ]
 )
