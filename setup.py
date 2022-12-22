4d04f57d080418f642d91655b8d156a015a6e7fa
adrian_egli <adrian.egli@gmail.com>
2022-12-22 09:31:04 +0100

    pip installation bug fix

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 
 setup(
     name='flatland-railway-extension',
-    version='0.1.3',
+    version='0.1.5',
     author='Adrian Egli',
     author_email="3dhelp@gmail.com",
     description='Extends Flatland Railway Simulator with helpful features.',
@@ -16,7 +16,6 @@ setup(
     python_requires='>=3.6, <4',
     packages=find_packages('.'),
     install_requires=[
-        'flatland-rl @ git+http://gitlab.aicrowd.com/flatland/flatland.git',
         'numpy',
         'matplotlib'
     ],
