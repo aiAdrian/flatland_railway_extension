cc492e90b0afc837f9870832e81ceed2c9d59427
adrian_egli <adrian.egli@gmail.com>
2022-12-22 08:32:14 +0100

    pip installation bug fix

--- setup.py
+++ setup.py
@@ -2,25 +2,22 @@
 # pip install twine
 # twine upload dist/*
 
-import os
-
 from setuptools import find_packages
 from setuptools import setup
 
-
 setup(
     name='flatland-railway-extension',
-    version='0.1.2',
+    version='0.1.3',
     author='Adrian Egli',
-    author_email = "3dhelp@gmail.com",
+    author_email="3dhelp@gmail.com",
     description='Extends Flatland Railway Simulator with helpful features.',
     url='https://github.com/aiAdrian/flatland_railway_extension',
     keywords='flatland, railway, extension, dynamics, simulation, multi-agent, reinforcement learning',
     python_requires='>=3.6, <4',
     packages=find_packages('.'),
     install_requires=[
-        'flatland-rl>=3.0.15',
+        'flatland-rl @ git+http://gitlab.aicrowd.com/flatland/flatland.git',
         'numpy',
-        'matplotlib',
-    ]
+        'matplotlib'
+    ],
 )
